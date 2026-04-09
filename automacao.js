75e6ee6f532d1af7d242f7882e74039d7c3ce88a
claroca07 <anaclara123.limaandre@gmail.com>
2026-04-09 11:58:52 -0300

    Mudando lógica para adicionar cores novas de acordo com a tag; tags repetidas terão a mesma cor

--- automacao.js
+++ automacao.js
@@ -1,37 +1,42 @@
 function atribuiBordaAleatoria() {
-        const elementos = document.querySelectorAll('body *:not(script):not(style)');
-        const qtdElementos = elementos.length;
+    const elementos = document.querySelectorAll('body *:not(script):not(style)');
+    const qtdElementos = elementos.length;
+    const coresTag = {};
 
-        for (let i = 0; i < qtdElementos; i++) {
-            const elemento = elementos[i];
+    for (let i = 0; i < qtdElementos; i++) {
+        const elemento = elementos[i];
+        const tag = elemento.tagName.toLowerCase();
 
-            const matiz = (360 / qtdElementos) * i;
-            const cor = `hsl(${matiz}, 100%, 50%)`;
-
-            elemento.style.border = `3px solid ${cor}`;
-            elemento.style.position = 'relative';
-            elemento.style.padding = '10px'; // apenas para fim de melhor visualização
-
-
-            const tagNome = document.createElement('span');
-            tagNome.innerText = elemento.tagName.toLowerCase();
-
-            Object.assign(tagNome.style, {
-                position: 'absolute',
-                top: '0',
-                left: '0',
-                backgroundColor: cor,
-                color: '#000',
-                fontSize: '10px',
-                padding: '2px 5px',
-                fontWeight: 'bold',
-                zIndex: '1000',
-                pointerEvents: 'none',
-                lineHeight: '1'
-            });
-
-            elemento.prepend(tagNome);
+        if (!coresTag[tag]) {
+            const matiz = (Object.keys(coresTag).length * 36) % 360;
+            coresTag[tag] = `hsl(${matiz}, 100%, 50%)`;
         }
-    }
 
-    window.onload = atribuiBordaAleatoria;
+        const cor = coresTag[tag];
+
+        elemento.style.border = '2px solid ' + cor;
+        elemento.style.position = 'relative';
+        elemento.style.padding = '10px'; // apenas para fim de melhor visualização
+
+        const tagNome = document.createElement('span');
+        tagNome.innerText = elemento.tagName.toLowerCase();
+
+        Object.assign(tagNome.style, {
+            position: 'absolute',
+            top: '0',
+            left: '0',
+            backgroundColor: cor,
+            color: '#000',
+            fontSize: '10px',
+            padding: '2px 5px',
+            fontWeight: 'bold',
+            zIndex: '1000',
+            pointerEvents: 'none',
+            lineHeight: '1'
+        });
+
+        elemento.prepend(tagNome);
+    }
+}
+
+window.onload = atribuiBordaAleatoria;
